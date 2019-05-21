68f6992cff509437a7a55156060cf66a0744f2d0
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2019-05-22 04:50:24 +0530

    added social media links

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -6,11 +6,22 @@ class Landing extends Component{
     render(){
         return(
             <div className="land">
+                <div className="social-nav">
+                <Grid container className="socialNavGrid" spacing={0}>
+                   <Grid item xs><a href="www.google.com/schezeen"><i className="social-icon fab fa-linkedin fa-lg"></i></a></Grid>
+                   <Grid item xs><a href="www.google.com"><i className="social-icon fab fa-medium fa-lg"></i></a></Grid>
+                   <Grid item xs><a href="www.google.com"><i className="social-icon fab fa-twitter fa-lg"></i></a></Grid>
+                   <Grid item xs><a href="www.google.com"><i className="social-icon fab fa-facebook fa-lg"></i></a></Grid>
+                   <Grid item xs><a href="www.google.com"><i className="social-icon fab fa-github fa-lg"></i></a></Grid>
+               </Grid>
+                </div>
+              
                <Grid container className = "landingGrid">
                     <Grid item xs = {12}>
                         <img alt="avatar" className="avatarImage" src={require("./../imageData/schez.png")}/>
                         <div className="nameBanner">
-                            <h1>Schezeen Fazulbhoy</h1>
+                            <h1 className="fullName">Schezeen Fazulbhoy</h1>
+                            <p>Deeply invested in development, design and documentation, I am constantly striving to channelize these aspects in ways that they complement each other.</p>
                         </div>
                     </Grid>
                </Grid>
